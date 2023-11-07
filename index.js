40ce0726d07c4adde57bfdeb7b4324a3c97bba26
ashimroyrupok <ashimroyrupok@gmail.com>
2023-11-08 00:41:59 +0600

    get jobs data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
-
-console.log(process.env.DB_USER);
-
 
 
 
@@ -32,6 +29,59 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+
+        const jobsCollection = client.db("jobsCollection").collection('jobs')
+        const BidjobsCollection = client.db("jobsCollection").collection('bidJobs')
+
+        
+        // posted job
+
+        app.post("/jobs" , async(req,res) => {
+            const jobs = req.body
+            const result= await jobsCollection.insertOne(jobs)
+            res.send(result)
+        })
+
+        app.get('/jobs', async(req,res)=>{
+            const result = await jobsCollection.find().toArray();
+            res.send(result)
+            
+        })
+
+
+
+        // app.get('/jobs/:id', async(req,res ) => {
+        //     const id = req.params.id;
+        //     const query = {_id : new ObjectId(id)}
+        //     const result = await jobsCollection.findOne(query)
+        //     res.send(result)
+        // })
+
+
+        app.get('/jobs/:id', async(req,res ) => {
+            const userEmail = req.params.id;
+            const query = {BuyerEmail : userEmail}
+            const result = await jobsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // bid job collection
+
+        app.post("/bidJobs" , async(req,res) => {
+            const jobs = req.body
+            const result= await BidjobsCollection.insertOne(jobs)
+            res.send(result)
+        })
+
+
+        app.get('/bidJobs', async(req,res)=>{
+            const result = await BidjobsCollection.find().toArray();
+            res.send(result)
+            
+        })
+
+ 
+
 
         
 
